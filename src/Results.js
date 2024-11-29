dcc9161acbc1f9504420909d37087cab26ffec63
HildaAb <ilona.aase@gmail.com>
2024-11-29 22:12:36 +0100

    Added word definition

---
+++ src/Results.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Meaning from "./Meaning";
+import "./Results.css";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="results">
+        <section>
+          <h2 className="word">{props.results.word}</h2>
+          <h2 className="phonetics">/{props.results.phonetic}/</h2>
+        </section>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <section key={index}>
+              <Meaning meaning={meaning} />
+            </section>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
