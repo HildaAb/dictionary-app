7f2c3efd250dc65fc22041187d8f1bdecdb33f81
HildaAb <ilona.aase@gmail.com>
2024-11-30 14:03:50 +0100

    added phonetic files

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import "./Results.css";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
@@ -8,7 +9,13 @@ export default function Results(props) {
       <div className="results">
         <section>
           <h2 className="word">{props.results.word}</h2>
-          <h2 className="phonetics">/{props.results.phonetic}/</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
         </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
