8cab852188397f784d9bbbc0c1ef0a65c1fa7104
HildaAb <ilona.aase@gmail.com>
2024-11-30 16:07:05 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,29 @@ export default function App() {
         <p>Dictionary</p>
         <Dictionary />
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/HildaAb" target="_blank" rel="noreferrer">
+          Ilona Aase
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/HildaAb/dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on GitHub{" "}
+        </a>{" "}
+        and{" "}
+        <a
+          href=" https://dictionary-for-me.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          hosted on Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }
