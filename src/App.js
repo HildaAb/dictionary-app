34395557eb016e476b02b658b9d4660605eec724
HildaAb <ilona.aase@gmail.com>
2024-12-01 17:47:59 +0100

    adjusted footer and css

--- src/App.js
+++ src/App.js
@@ -11,30 +11,31 @@ export default function App() {
         <h5>English</h5>
         <h1>Dictionary</h1>
         <Dictionary />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/HildaAb" target="_blank" rel="noreferrer">
+            Ilona Aase
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/HildaAb/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            on GitHub{" "}
+          </a>{" "}
+          and{" "}
+          <a
+            href=" https://dictionary-for-me.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify{" "}
+          </a>
+          <br />
+        </footer>
       </div>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/HildaAb" target="_blank" rel="noreferrer">
-          Ilona Aase
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/HildaAb/dictionary-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          on GitHub{" "}
-        </a>{" "}
-        and{" "}
-        <a
-          href=" https://dictionary-for-me.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          hosted on Netlify{" "}
-        </a>
-      </footer>
     </div>
   );
 }
