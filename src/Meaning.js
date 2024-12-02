e36711071b383dc5f537d45e870afb60d2ac411b
HildaAb <ilona.aase@gmail.com>
2024-12-02 16:49:55 +0100

    adjuted the number av answer and made more responsive

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,13 +6,14 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
+      {props.meaning.definitions.slice(0, 3).map(function (definition, index) {
         return (
           <div key={index}>
             <div className="definition">{definition.definition}</div>
-
-            <div className="example">{definition.example}</div>
-            <Synonyms synonyms={definition.synonyms} />
+            {definition.example && (
+              <div className="example">Example: {definition.example}</div>
+            )}
+            <Synonyms synonyms={definition.synonyms} />{" "}
           </div>
         );
       })}
